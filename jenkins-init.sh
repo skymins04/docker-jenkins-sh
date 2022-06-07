51376f69f6fb296f0d28d52f4569c9daf61c7732
skymins04 <skymin0417@gmail.com>
2022-06-08 08:49:04 +0900

    jenkins-init.sh ~/jenkins 디렉터리 권한 문제 수정

--- jenkins-init.sh
+++ jenkins-init.sh
@@ -1,8 +1,4 @@
 mkdir -p ~/jenkins
-<<<<<<< HEAD
-sudo chown -R 1000:1000 ~/jenkins
-=======
 sudo chmod 777 ~/jenkins
->>>>>>> 123ed453
 sudo docker build --no-cache -t jenkins . && \
 sudo docker-compose up -d
