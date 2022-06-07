77330f9984491a2f769dd3614d4d391509f39a79
skymins04 <skymin0417@gmail.com>
2022-06-07 23:58:50 +0900

    jenkins-init.sh 수정

--- jenkins-init.sh
+++ jenkins-init.sh
@@ -1,7 +1,9 @@
+mkdir -p ~/jenkins
 sudo chmod +x /usr/bin/docker
 sudo chmod +x /usr/bin/docker-compose
 sudo chmod +x /var/run/docker.sock
 sudo docker run -d --name jenkins -p 50000:8080 \
+ -v ~/jenkins:/var/jenkins_home \
  -v /var/run/docker.sock:/var/run/docker.sock \
  -v /usr/bin/docker:/usr/bin/docker \
  -v /usr/bin/docker-compose:/usr/bin/docker-compose \
